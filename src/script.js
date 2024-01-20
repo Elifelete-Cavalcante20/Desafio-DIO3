a818aa59222df0c26462433c354734103f10fa22
Elifelete-Cavalcante20 <elif.cav00@gmail.com>
2024-01-20 11:55:12 -0300

    Terceiro commit (adicionada a logica de tomada de decisao)

--- src/script.js
+++ src/script.js
@@ -1,18 +1,39 @@
 class hero { 
-    constructor (name, type, age, blow){
+    constructor (name, type, age,){
         this.name = name
         this.type = type
         this.age = age
-        this.blow = blow
+        
     }
     atak(){ 
-        
-        console.log(`O ${this.type}, ${this.name} ${this.blow}`)
-    }
-}
 
-let monge = new hero ("Tsubasa", "Monge", "125","usou artes marciais")
-let guerreiro = new hero ("Haaland", "Guerreiro", "23", "usou espada")
+     let blow;
+
+       switch (this.type){
+        case "Mago": 
+            blow = "usando magia"
+            break;
+        case "Guerreiro":
+            blow = "usando espada"
+            break;
+        case "Monge":
+            blow = "usando artes marcias"
+            break;
+        case "Ninja": 
+            blow = "usando shuriken"
+            break;
+       }
+       console.log(`O ${this.type} chamado, ${this.name}, atacou ${blow}`)
+    }
+ }
+
+
+let monge = new hero ("Tsubasa", "Monge", "125")
+let guerreiro = new hero ("Haaland", "Guerreiro", "23")
+let ninja = new hero ("Yasu", "Ninja", "45")
+let mago = new hero ("Dumbledor", "Mago", "367")
 
 monge.atak()
 guerreiro.atak()
+monge.atak()
+mago.atak()
